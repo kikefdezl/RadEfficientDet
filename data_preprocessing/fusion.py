f4cccda571038663d0e0898ac1495e0f0df35292
kikefdezl <enriquelagui@outlook.com>
2021-06-07 12:06:19 +0200

    added nuscenes devkit

--- data_preprocessing/fusion.py
+++ data_preprocessing/fusion.py
@@ -4,5 +4,15 @@ data_dir = "D:/MULTIMEDIA/NuScenes"
 
 nusc = NuScenes(version='v1.0-mini', dataroot=data_dir, verbose=True)
 
+all_samples = []
 
+for scene in nusc.scene:
+    first_sample_token = scene['first_sample_token']
+    curr_sample = nusc.get('sample', first_sample_token)
+
+    for _ in range(scene['nbr_samples']-1):
+        all_samples.append(curr_sample)
+        next_token = curr_sample['next']
+        curr_sample = nusc.get('sample', next_token)
+    all_samples.append(curr_sample)  # this appends the last sample of the scene
 
