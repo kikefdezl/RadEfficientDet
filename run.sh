0a841faef2e2f1f2bcdc939c9859d38b839bcc58
kikefdezl <enriquelagui@outlook.com>
2022-03-06 23:22:30 +0100

    modified EfficientDet architecture to include Radar maps

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-python3 EfficientRaDet/train.py \
---snapshot "/mnt2/ITP_PROJECT/PRETRAINED_MODELS/efficientdet-d0.h5" \
+python3 RadEfficientDet/train.py \
+--snapshot "/mnt/TFM_KIKE/PRETRAINED_MODELS/efficientdet-d0.h5" \
 --weighted-bifpn \
 --phi 0 \
 --gpu 0 \
@@ -7,10 +7,12 @@ python3 EfficientRaDet/train.py \
 --compute-val-loss \
 --freeze-backbone \
 --batch-size 8 \
---epochs 300 \
+--epochs 103 \
 --steps -1 \
+--workers 4 \
+--max-queue-size 64 \
 --tensorboard-dir \
-"/mnt/TFM_KIKE/EXPERIMENTS_KERAS/EXP_D0_RAW_IMGS/" \
+"/mnt/TFM_KIKE/EXPERIMENTS_KERAS/EXP_D0_RADAR_MAPS/" \
 csv \
 "/mnt/TFM_KIKE/DATASETS/fused_imgs_v3_no_visibility_0/train.csv" \
 "/mnt/TFM_KIKE/DATASETS/fused_imgs_v3_no_visibility_0/dataset_encoding.csv" \
