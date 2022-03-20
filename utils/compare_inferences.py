714da9fc8a97a7e21f85fc835d1f9f9cbd645e26
kikefdezl <enriquelagui@outlook.com>
2022-03-20 18:13:28 +0100

    added compare_inferences.py

---
+++ utils/compare_inferences.py
@@ -0,0 +1,22 @@
+import cv2
+from tqdm import tqdm
+import os
+
+def main():
+    camera_images_dir = "/mnt/TFM_KIKE/INFERENCES/cam_inf_on_rain/"
+    radar_images_dir = "/mnt/TFM_KIKE/INFERENCES/rad_inf_on_rain/"
+    save_dir = "/mnt/TFM_KIKE/INFERENCES/inf_merged/"
+
+    for image in tqdm(os.listdir(radar_images_dir)):
+        image_cam_path = os.path.join(camera_images_dir, image)
+        image_radar_path = os.path.join(radar_images_dir, image)
+
+        image_cam = cv2.imread(image_cam_path)
+        image_radar = cv2.imread(image_radar_path)
+
+        merged_image = cv2.hconcat([image_cam, image_radar])
+
+        cv2.imwrite(os.path.join(save_dir, image), merged_image)
+
+if __name__ == "__main__":
+    main()
