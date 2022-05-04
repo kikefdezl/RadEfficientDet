f1cca92b0f41156b5f17b7f196bc949339412f35
kikefdezl <enriquelagui@outlook.com>
2022-05-04 09:57:25 +0200

    added radar mode to run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 python3 RadEfficientDet/train.py \
 --snapshot "/mnt/TFM_KIKE/PRETRAINED_MODELS/efficientdet-d0.h5" \
 --weighted-bifpn \
+--radar-mode "concat" \
 --phi 0 \
---gpu 0 \
+--gpu 1 \
 --random-transform \
 --compute-val-loss \
 --freeze-backbone \
@@ -12,9 +13,9 @@ python3 RadEfficientDet/train.py \
 --workers 4 \
 --max-queue-size 64 \
 --tensorboard-dir \
-"/mnt/TFM_KIKE/EXPERIMENTS_KERAS/EXP_D0_RADAR_MAPS/" \
+"/mnt/TFM_KIKE/EXPERIMENTS_KERAS/EXP_D0_CONCAT_V3_OV4/" \
 csv \
-"/mnt/TFM_KIKE/DATASETS/fused_imgs_v3_no_visibility_0/train.csv" \
-"/mnt/TFM_KIKE/DATASETS/fused_imgs_v3_no_visibility_0/dataset_encoding.csv" \
+"/mnt/TFM_KIKE/DATASETS/fused_imgs_v5_only_visibility_4/train.csv" \
+"/mnt/TFM_KIKE/DATASETS/fused_imgs_v5_only_visibility_4/dataset_encoding.csv" \
 --val-annotations-path \
-"/mnt/TFM_KIKE/DATASETS/fused_imgs_v3_no_visibility_0/val.csv"
+"/mnt/TFM_KIKE/DATASETS/fused_imgs_v5_only_visibility_4/val.csv"
